fa3fc79e1da4c2c44fc4152834d1a6d68903da88
moussasamina <moussa.malle@samina.tech>
2025-05-27 21:32:10 +0000

    commands.sh updated

--- commands.sh
+++ commands.sh
@@ -1,6 +1,10 @@
-# docker compose up -d
-# docker exec -i config1 mongosh < scripts/init-config-server.js
-# docker exec -i shard2-1 mongosh --port 27018 < scripts/init-shard2.js
-# docker exec -i shard3-1 mongosh --port 27018 < scripts/init-shard3.js
-# docker exec -i shard1-1 mongosh --port 27018 < scripts/init-shard1.js
+echo "Lunch cluster started...."
+docker compose up -d
+docker exec -i config1 mongosh < scripts/init-config-server.js
+docker exec -i shard2-1 mongosh --port 27018 < scripts/init-shard2.js
+docker exec -i shard3-1 mongosh --port 27018 < scripts/init-shard3.js
+docker exec -i shard1-1 mongosh --port 27018 < scripts/init-shard1.js
 # docker exec -i projet-mongos mongosh < scripts/add-shards.js
+
+echo "End."
+
